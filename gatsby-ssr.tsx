031d250efebe4ae210bed342fcff588c9af0fa3b
Lukasz  T <lukasz.trzos1@gmail.com>
2023-02-24 22:16:16 +0100

    add gatsby-ssr

---
+++ gatsby-ssr.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import type { GatsbySSR } from 'gatsby'
+import './src/assets/styles/global.scss'
+import MainTemplate from 'src/components/MainTemplate'
+
+export const wrapPageElement: GatsbySSR['wrapPageElement'] = ({ element }) => {
+  return <MainTemplate>{element}</MainTemplate>
+}
