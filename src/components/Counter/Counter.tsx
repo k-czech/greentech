66323b0853a81602dcfb3a488ddd046c229f0c69
Lukasz  T <lukasz.trzos1@gmail.com>
2023-03-01 22:01:21 +0100

    updated footer, added counter

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -3,6 +3,8 @@ import { Button } from '../Button/Button'
 import Title from '../Title/Title'
 import Wrapper from '../Wrapper/Wrapper'
 import { graphql } from 'gatsby'
+import { useInView } from 'react-intersection-observer'
+import CountUp from 'react-countup'
 
 interface Props {
   title: string
@@ -14,6 +16,11 @@ interface Props {
 }
 
 const Counter = ({ title, datas, link, linkText }: Props) => {
+  const [ref, inView] = useInView({
+    threshold: 0.3,
+    triggerOnce: true,
+  })
+
   const arr = datas.datas.split('-').map((item) => {
     const subarr = item.split(';')
     return {
@@ -22,16 +29,23 @@ const Counter = ({ title, datas, link, linkText }: Props) => {
     }
   })
 
+  // <span className='text-white text-6xl font-extrabold pb-2 block' ref={countUpRef} />
+
   return (
     <section className={`bg-yellow-color pt-24 pb-16`} id="counter">
       <Wrapper className="mx-auto lg:space-y-16">
         <Title text={title} className={`text-white`} />
         <div className="flex flex-col md:flex-row md:justify-between md:items-start space-y-5 md:space-y-0">
           {arr.map((item, index) => (
-            <div key={index}>
-              <span className={`text-white text-6xl font-extrabold pb-2 block`}>
-                {item.num}
-              </span>
+            <div key={index} ref={ref}>
+              <CountUp start={0} end={inView ? item.num : 0} duration={3}>
+                {({ countUpRef }) => (
+                  <span
+                    className="text-white text-6xl font-extrabold pb-2 block"
+                    ref={countUpRef}
+                  />
+                )}
+              </CountUp>
               <p className={`text-white text-base font-light max-w-[200px]`}>
                 {item.text}
               </p>
